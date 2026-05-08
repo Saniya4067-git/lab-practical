0a19cfcc83cf87828de1561f59412cd9c7be2071
Saniya4067-git <24013067@pvgcoet.ac.in>
2026-05-08 17:01:25 +0530

    Add files via upload

---
+++ booking-app/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Serve static files from "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
+
+//npm init -y
+//npm install express
+//node server.js
